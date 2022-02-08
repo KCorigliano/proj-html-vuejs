e2929f7296b2a7d64bd0b02d57d57903613b5524
KCorigliano <corigliano98@hotmail.com>
2022-02-08 18:04:09 +0100

    footer style

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -8,10 +8,10 @@
                 <p>+1 (305) 547-9909 (9am -5 am EST, Monday - Fryday)</p>
                 <p>support@maxcoach.com</p>
                 <div class="social-row">
-                    <p>facebook</p>
-                    <p>facebook</p>
-                    <p>facebook</p>
-                    <p>facebook</p>
+                    <p><font-awesome-icon class="icon" :icon="['fab', 'facebook-square']" @click="searchInputShow" /></p>
+                    <p><font-awesome-icon class="icon" :icon="['fab', 'twitter']" @click="searchInputShow" /></p>
+                    <p><font-awesome-icon class="icon" :icon="['fab', 'instagram']" @click="searchInputShow" /></p>
+                    <p><font-awesome-icon class="icon" :icon="['fab', 'linkedin']" @click="searchInputShow" /></p>
                 </div>
             </div>
             <div>
@@ -20,17 +20,31 @@
                     <p v-for="(link, i) in exploreLinks" :key="i">{{link}}</p>
                 </div>
             </div>
-            <div>
+            <div class="right-links">
                 <h4>Information</h4>
                 <p v-for="(link, i) in informationLinks" :key="i">{{link}}</p>
             </div>
+        </div>
+        <div class="rights-explication">
+            <p>©2020 Maxcoach- All Rights Reserved</p>
         </div>
     </div>
 </div>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebookSquare, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faFacebookSquare);
+library.add(faInstagram);
+library.add(faTwitter);
+library.add(faLinkedin);
+
 export default {
+    components:{
+        FontAwesomeIcon
+    },
     props:{
         exploreLinks: Array,
         informationLinks: Array,
@@ -39,26 +53,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .container-fluid{
     width: 100%;
     background-color: #141423;
     color: white;
     padding: 50px 0;
+
     .container{
         width: 1200px;
         margin: 0 auto;
+
+        p{
+            color: grey;
+            margin: 15px 0 0 0;
+        }
     
         .row{
             display: flex;
             justify-content: space-between;
-    
-            .social-row{
-                display: flex;
-    
-                p{
-                    margin-right: 15px;
+
+            .contacts{
+
+                .social-row{
+                    display: flex;
+                    color: grey;
+                    justify-content: space-between;
+                    width: 150px;
+                    margin-top: 30px;
+                    font-size: 20px;
                 }
             }
+    
     
             .middle-links{
                 display: flex;
@@ -70,11 +96,19 @@ export default {
                     cursor: pointer;
                 }
             }
-    
-            p{
-                color: grey;
-                margin: 15px 0 0 0;
+
+            .right-links{
+                
+                p{
+                    cursor: pointer;
+                }
             }
+    
+        }
+
+        .rights-explication{
+            text-align: center;
+            margin-top: 100px;
         }
     }
 }
