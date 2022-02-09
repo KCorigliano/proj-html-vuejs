a722aa03622e1bf1761f0aa87f893a3672582f3f
KCorigliano <corigliano98@hotmail.com>
2022-02-09 14:08:23 +0100

    row 7 style

--- src/components/Row-7-LatestBlog.vue
+++ src/components/Row-7-LatestBlog.vue
@@ -1,37 +1,38 @@
 <template>
 <div class="container">
-    <h1>Latest Online Courses</h1>
+    <h5>READ FOR MORE JOYMENT</h5>
+    <h1>Latest From <span>Our Blogs</span></h1>
     <div class="row">
         <div class="courses-container">
             <img src="../assets/images/artist-blog-03-480x325.jpeg" alt="">
             <div class="courses-text">
-                <h2>ARTIST</h2>
+                <h5>ARTIST</h5>
                 <p>Brush Strokes Energize Trees in Paintings</p>
                 <div>
-                    <span>May 15, 2020</span>
-                    <span>688 views</span>
+                    <span><font-awesome-icon class="icon" icon="calendar" />May 15, 2020</span>
+                    <span><font-awesome-icon class="icon" icon="eye" />688 views</span>
                 </div>
             </div>
         </div>
         <div class="courses-container">
             <img src="../assets/images/artist-blog-01-480x325.jpg" alt="">
             <div class="courses-text">
-                <h2>ARTIST</h2>
+                <h5>ARTIST</h5>
                 <p>Pocket-Sized Notebooks Hold Miniature Paintings</p>
                 <div>
-                    <span>May 15, 2020</span>
-                    <span>603 views</span>
+                    <span><font-awesome-icon class="icon" icon="calendar" />May 15, 2020</span>
+                    <span><font-awesome-icon class="icon" icon="eye" />603 views</span>
                 </div>
             </div>
         </div>
         <div class="courses-container">
             <img src="../assets/images/artist-blog-02-480x325.jpg" alt="">
             <div class="courses-text">
-                <h2>ARTIST</h2>
+                <h5>ARTIST</h5>
                 <p>Connection Between Self-Portraits and Identity</p>
                 <div>
-                    <span>May 15, 2020</span>
-                    <span>397 views</span>
+                    <span><font-awesome-icon class="icon" icon="calendar" />May 15, 2020</span>
+                    <span><font-awesome-icon class="icon" icon="eye" />397 views</span>
                 </div>
             </div>
         </div>
@@ -40,16 +41,36 @@
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCalendar, faEye } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCalendar);
+library.add(faEye);
+
 export default {
-    
+    components:{
+        FontAwesomeIcon
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/variables.scss';
 .container{
+
+    h5{
+        text-align: center;
+        color: $delta;
+        margin-bottom: 25px;
+        font-weight: 400;
+    }
 
     h1{
         text-align: center;
+        span{
+            color: $montainMeadow;
+            font-weight: 200;
+        }
     }
     
     .row{
@@ -72,6 +93,11 @@ export default {
                 border-radius: 0 0 5px 5px;
                 margin-top: -5px;
 
+                h5{
+                    text-align: left;
+                    margin-bottom: 5px;
+                }
+
                 p{
                     margin: 15px 0 30px;
                     font-weight: bold;
@@ -80,12 +106,15 @@ export default {
     
                 span{
                     margin-right: 30px;
-                    color: grey;
+                    color: $delta;
                     transition: all 0.5s;
+                    .icon{
+                        margin-right: 5px;
+                    }
                 }
 
                 &:hover{
-                    box-shadow: 0 0 10px grey;
+                    box-shadow: 0 0 10px $delta;
                     cursor: pointer;
                     transition: all 0.5s;
                 }
